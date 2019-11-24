561aabfac3b8e9934355b8197bf3013d14e9c495
moody <justin.n.moody@gmail.com>
2019-11-24 00:34:14 -0500

    add currency consts and component

---
+++ src/components/CurrencyIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <img :src="`./assets/${currency}.svg`" :width="size" :height="size" />
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { CurrencyType } from "../consts";
+
+@Component
+export default class CurrencyIcon extends Vue {
+  @Prop(String) private currency!: CurrencyType;
+  @Prop({ type: [Number, String], default: 40 }) private size?: number | string;
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/consts.ts
@@ -0,0 +1,68 @@
+export type CurrencyType =
+  | "AUD"
+  | "BGN"
+  | "BRL"
+  | "CAD"
+  | "CHF"
+  | "CNY"
+  | "CZK"
+  | "DKK"
+  | "GBP"
+  | "HKD"
+  | "HRK"
+  | "HUF"
+  | "IDR"
+  | "ILS"
+  | "INR"
+  | "ISK"
+  | "JPY"
+  | "KRW"
+  | "MXN"
+  | "MYR"
+  | "NOK"
+  | "NZD"
+  | "PHP"
+  | "PLN"
+  | "RON"
+  | "RUB"
+  | "SEK"
+  | "SGD"
+  | "THB"
+  | "TRY"
+  | "USD"
+  | "ZAR";
+
+export const Currencies: { [key in CurrencyType]: string } = {
+  AUD: "Australian Dollar",
+  BGN: "Bulgarian Lev",
+  BRL: "Brazilian Real",
+  CAD: "Canadian Dollar",
+  CHF: "Swiss Franc",
+  CNY: "Chinese Yuan",
+  CZK: "Czech Koruna",
+  DKK: "Danish Krone",
+  GBP: "British Pound",
+  HKD: "Hong Kong Dollar",
+  HRK: "Croatian Kuna",
+  HUF: "Hungarian Forint",
+  IDR: "Indonesian Rupiah",
+  ILS: "Israeli New Shekel",
+  INR: "Indian Rupee",
+  ISK: "Icelandic Króna",
+  JPY: "Japanese Yen",
+  KRW: "South Korean Won",
+  MXN: "Mexican Peso",
+  MYR: "Malaysian Ringgit",
+  NOK: "Norwegian Krone",
+  NZD: "New Zealand Dollar",
+  PHP: "Philippine Peso",
+  PLN: "Polish Zloty",
+  RON: "Romanian Leu",
+  RUB: "Russian Ruble",
+  SEK: "Swedish Krona",
+  SGD: "Singapore Dollar",
+  THB: "Thai Baht",
+  TRY: "Turkish lira",
+  USD: "United States Dollar",
+  ZAR: "South African Rand"
+};
