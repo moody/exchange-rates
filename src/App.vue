a3dd57c8b0bb3640a32e2ab8c989c61d35dfede9
moody <justin.n.moody@gmail.com>
2019-11-30 16:58:20 -0500

    setup basic ui for api data

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,35 @@
       </v-btn>
     </v-app-bar>
 
-    <v-content>TODO...</v-content>
+    <v-content>
+      <v-container grid-list-xl wrap>
+        <v-layout row wrap>
+          <v-flex
+            xs12
+            md6
+            lg4
+            xl3
+            v-for="item in rateData"
+            :key="item.currency"
+          >
+            <v-card>
+              <v-card-title class="mb-1">
+                <currency-icon :currency="item.currency" size="30" />
+                <span class="ml-2">{{ item.currency }}</span>
+              </v-card-title>
+
+              <v-card-subtitle>
+                {{ item.name }}
+              </v-card-subtitle>
+
+              <v-card-text>
+                1 {{ baseCurrency }} = {{ item.rate }} {{ item.currency }}.
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
 
     <v-footer>
       <v-spacer></v-spacer>
@@ -26,7 +54,24 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { CurrencyModule } from "@/store/modules/currency";
+import { CurrencyType } from "@/types";
+import CurrencyIcon from "@/components/CurrencyIcon.vue";
 
-@Component({ name: "App", components: {} })
-export default class App extends Vue {}
+@Component({
+  name: "App",
+  components: { CurrencyIcon },
+  created() {
+    CurrencyModule.fetch();
+  }
+})
+export default class App extends Vue {
+  get baseCurrency() {
+    return CurrencyModule.baseCurrency;
+  }
+
+  get rateData() {
+    return CurrencyModule.rateData;
+  }
+}
 </script>
